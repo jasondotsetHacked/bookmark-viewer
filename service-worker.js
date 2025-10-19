7c0aceea12fa3ca418a356b81c6e9de242fe7603
setHacked <sethacked@gmail.com>
2025-10-18 21:32:22 -0400

    fix: Ensure service worker skips waiting and cleans up old caches properly

--- service-worker.js
+++ service-worker.js
@@ -21,6 +21,7 @@ const ASSET_PATHS = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
@@ -45,14 +46,17 @@ self.addEventListener('fetch', event => {
 self.addEventListener('activate', event => {
     const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames.map(cacheName => {
+                        if (!cacheWhitelist.includes(cacheName)) {
+                            return caches.delete(cacheName);
+                        }
+                        return Promise.resolve();
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
     );
 });
